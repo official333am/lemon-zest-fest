d1691c260409a9d3001c2e824040a2e440a4dc5b
Rohith Sebastian <rohithsebastian@rohiths-mbp.myfiosgateway.com>
2021-04-04 17:38:46 -0400

    update - %date% %time%

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public profiles = [
+    {
+      facebook: 'https://www.instagram.com/lemonzestfest/',
+      instagram: 'https://www.instagram.com/lemonzestfest/',
+      profilePath: '../../assets/images/profiles/sana.jpg',
+      role: 'Chief Lemon Zester',
+      name: 'Sana',
+      twitter: 'https://www.instagram.com/lemonzestfest/'
+    },
+    {
+      facebook: 'https://www.instagram.com/lemonzestfest/',
+      instagram: 'https://www.instagram.com/lemonzestfest/',
+      profilePath: '../../assets/images/profiles/mike.jpg',
+      role: 'Executive Lemon Squeezer',
+      name: 'Mike',
+      twitter: 'https://www.instagram.com/lemonzestfest/'
+    },
+    {
+      facebook: 'https://www.instagram.com/lemonzestfest/',
+      instagram: 'https://www.instagram.com/lemonzestfest/',
+      profilePath: '../../assets/images/profiles/rohith.jpg',
+      role: 'Head of Lemonheads',
+      name: 'Rohith',
+      twitter: 'https://www.instagram.com/lemonzestfest/'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
